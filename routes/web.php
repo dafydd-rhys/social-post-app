c1b204267bab3509d649c8f8947a4bea8a80bba2
dafydd-rhys <maund.dafyddrhys@gmail.com>
2024-03-13 21:15:48 +0000

    Added routes for website

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/post/{id?}', function ($post_id) {
+    return view('post');
+});
+
+Route::get('/user/{user_id?}', function ($user_id) {
+    return view('user');
+});
+
+Route::get('/user/{user_id}/comments', function ($user_id) {
+    return view('user_comments');
+});
+
+Route::get('/user/{user_id}/comments/{comment_id}', function ($user_id, $comment_id) {
+    return view('comment');
+});
+
+Route::get('/account/{user_id?}', function ($user_id) {
+    return view('account');
+});
+
+Route::get('/login', function ($user_id) {
+    return view('login');
+});
+
+Route::get('/register', function ($user_id) {
+    return view('register');
 });
